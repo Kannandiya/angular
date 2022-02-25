d892b132f68995ac903af3dffa3e881758eb685e
@KannanM1312 <kannandiya20002@gmail.com>
2022-02-25 10:33:36 +0530

    hai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  status: boolean = false;
+  clickEvent(){
+      this.status = !this.status;       
+  } 
+  
   title = 'hrms';
 }
